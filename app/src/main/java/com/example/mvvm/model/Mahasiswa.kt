2020a919d7e9885c300fc5434d4eccbfd87f3eae
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-11-18 11:48:39 +0700

    menamabah nim di data kelas

--- app/src/main/java/com/example/mvvm/model/Mahasiswa.kt
+++ app/src/main/java/com/example/mvvm/model/Mahasiswa.kt
@@ -3,6 +3,7 @@ package com.example.mvvm.model
 data class Mahasiswa(
     val nama : String="",
     val alamat : String="",
-    val gender : String=""
+    val gender : String="",
+    val nim    : String = ""
 )
 
